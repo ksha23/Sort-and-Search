2bd1cbb96a04fdad9b5714cc57ecfda15da825c0
ksha23 <74990237+ksha23@users.noreply.github.com>
2023-03-02 19:54:49 -0600

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,15 @@
+public class SelectionSort {
+	public static void sort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			int least = arr[i];
+			for (int j = i; j < arr.length; j++) {
+				if (arr[j] < least) {
+					least = arr[j];
+				}
+			}
+			int temp = arr[i];
+			arr[i] = arr[least];
+			arr[least] = temp;
+		}
+	}
+}
