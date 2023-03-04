9fa2424ab7601909b527eb028ee4cdadac3a05fc
ksha23 <74990237+ksha23@users.noreply.github.com>
2023-03-04 02:27:21 -0600

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,15 +1,29 @@
+import java.util.Arrays;
+
 public class SelectionSort {
 	public static void sort(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			int least = arr[i];
+		for (int i = 0; i < arr.length - 1; i++) {
+			int smallest = i;
 			for (int j = i; j < arr.length; j++) {
-				if (arr[j] < least) {
-					least = j;
+				if (arr[j] < arr[smallest]) {
+					smallest = j;
 				}
 			}
-			int temp = arr[i];
-			arr[i] = arr[least];
-			arr[least] = temp;
+				swap(i,smallest,arr);
 		}
+	}
+	
+	public static void swap(int a, int b, int[] arr) {
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {8,2,7,4,4,5,8,9,5,6,3,4,8,8,5,4,0,83};
+		System.out.println(Arrays.toString(arr));
+		System.out.println("------");
+		sort(arr);
+		System.out.println(Arrays.toString(arr));
 	}
 }
